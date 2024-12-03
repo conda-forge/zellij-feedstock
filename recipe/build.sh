4674973c237f72638e230854e1db963cbe3c1307
Daniel Nachun <daniel.nachun@gmail.com>
2024-12-02 22:45:40 -0800

    enable rattler-build

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,15 +2,15 @@
 
 set -o xtrace -o nounset -o pipefail -o errexit
 
+export CARGO_PROFILE_RELEASE_STRIP=symbols
+export CARGO_PROFILE_RELEASE_LTO=fat
+
 cargo-bundle-licenses \
     --format yaml \
     --output THIRDPARTY.yml
 
 # build statically linked binary with Rust
-cargo install --no-track --locked --root ${PREFIX} --path .
-
-# strip debug symbols
-"${STRIP}" "${PREFIX}/bin/${PKG_NAME}"
+cargo install --bins --no-track --locked --root ${PREFIX} --path .
 
 if [[ ${build_platform} == ${target_platform} ]]; then
     mkdir -p ${PREFIX}/etc/bash_completion.d
