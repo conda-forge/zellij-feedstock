30f3b27ab11659abfbe61d368a658bb5c4e39f4e
Daniel Nachun <daniel.nachun@gmail.com>
2024-12-15 19:42:08 -0800

    fix build

--- recipe/build.sh
+++ recipe/build.sh
@@ -12,8 +12,7 @@ cargo-bundle-licenses \
     --output THIRDPARTY.yml
 
 # build statically linked binary with Rust
-# cargo install --bins --no-track --locked --root ${PREFIX} --path .
-cargo xtask ci cross aarch64-apple-darwin
+cargo install --bins --no-track --locked --root ${PREFIX} --path .
 
 if [[ ${build_platform} == ${target_platform} ]]; then
     mkdir -p ${PREFIX}/etc/bash_completion.d
